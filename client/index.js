b28917417cff4c6f67b08e8a18ebadfffa511256
Aimable <niyoai@yahoo.fr>
2018-04-18 18:09:45 +0100

    created songs components

--- client/index.js
+++ client/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
+
+import App from './components/App';
+import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({});
 
@@ -9,7 +14,12 @@ const client = new ApolloClient({});
 const Root = () => {
   return (
       <ApolloProvider  client={client}>
-        <div>Lyrical</div>
+          <Router history={hashHistory}>
+              <Route path="/" component={App}>
+                  <IndexRoute component={SongList}/>
+                  <Route path="song/new" component={SongCreate}/>
+              </Route>
+          </Router>
       </ApolloProvider>
   );
 };
