af338ea838ea91158c2585fc8f1fdeebe33b9313
Aimable <niyoai@yahoo.fr>
2018-04-18 11:07:14 +0100

    Cloned file and components

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://aimable:database@ds247499.mlab.com:47499/lyricadbai';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
