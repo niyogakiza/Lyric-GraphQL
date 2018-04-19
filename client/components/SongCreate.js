cd9966f3772a431ba3c9f0f3ddf93dd2334bf7eb
Aimable <niyoai@yahoo.fr>
2018-04-19 18:43:58 +0100

    SOME CHANGES

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { Link, hashHistory} from 'react-router';
+
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component{
     constructor(props){
@@ -14,11 +17,11 @@ class SongCreate extends Component{
 
     onSubmit(event) {
         event.preventDefault();
+
         this.props.mutate({
-            variable: {
-                title: this.state.title
-            }
-        })
+            variables: { title: this.state.title },
+            refetchQueries:[{ query }]
+        }).then(() => hashHistory.push('/'))
     };
 
 
@@ -28,6 +31,7 @@ class SongCreate extends Component{
     render(){
         return(
             <div>
+                <Link to="/">Back</Link>
                 <h3>Create a new Song</h3>
                 <form onSubmit={this.onSubmit.bind(this)}>
                     <label>Song Title:</label>
